b38c76aed2429b1f2bac25ba001f1ade69fe0842
Simon <au649801@uni.au.dk>
2022-03-08 09:45:14 +0100

    removed now obsolete tests

--- RapidNJ_test.go
+++ RapidNJ_test.go
@@ -34,52 +34,12 @@ func Test4Taxa(t *testing.T) {
 
 	newick_result, _ := neighborJoin(D, S, labels, deadRecords, array, treeBanana)
 	print()
-	if newick_result != "((B:4.000000,A:13.000000):2.000000,(C:4.000000,D:10.000000):2.000000);" {
+
+	//note that the newick always becomes a rooted tree whereas our implementation of the algorithm generates an unrooted tree.
+	if newick_result != "(((A:13.000000,B:4.000000):4.000000,C:4.000000):5.000000,D:5.000000);" {
 		t.Errorf(newick_result)
 	}
 
-}
-
-func Test4Taxa_made_up_numbers(t *testing.T) {
-	labels := []string{
-		"A", "B", "C", "D",
-	}
-	D := [][]float64{
-		{0, 11, 18, 24},
-		{11, 0, 12, 18},
-		{18, 12, 0, 14},
-		{24, 18, 14, 0},
-	}
-	S, deadRecords, array, treeBanana := standardSetup(D, labels)
-
-	newick_result, _ := neighborJoin(D, S, labels, deadRecords, array, treeBanana)
-
-	if newick_result != "((B:2.500000,A:8.500000):2.750000,(C:4.000000,D:10.000000):2.750000);" {
-		t.Errorf("%s hehehe", newick_result)
-	}
-
-}
-
-func Test8Taxa_madeUpNumbers_shouldBeChangedLater(t *testing.T) {
-
-	labels := []string{"A", "B", "C", "D", "E", "F", "G", "H"}
-	D := [][]float64{
-		{0, 5, 68, 57, 127, 27, 28, 33},
-		{5, 0, 58, 47, 117, 8, 52, 57},
-		{68, 58, 0, 35, 69, 35, 87, 92},
-		{57, 47, 35, 0, 94, 44, 79, 84},
-		{127, 117, 69, 94, 0, 144, 149, 154},
-		{27, 8, 35, 44, 144, 0, 27, 54},
-		{28, 52, 87, 79, 149, 27, 0, 13},
-		{33, 57, 92, 84, 154, 54, 13, 0},
-	}
-
-	S, deadRecords, array, treeBanana := standardSetup(D, labels)
-
-	newick_result, _ := neighborJoin(D, S, labels, deadRecords, array, treeBanana)
-	if newick_result != "(B:5.765625,((((G:1.250000,H:11.750000):23.208333,A:0.791667):4.718750,F:29.781250):1.281250,((C:0.333333,E:68.666667):18.200000,D:11.800000):2.968750):5.765625);" {
-		t.Errorf("hehehe")
-	}
 }
 
 func Test_max_taxa_of_generated_tree(t *testing.T) {
@@ -240,12 +200,12 @@ func TestRuntimeOfBigTaxas(t *testing.T) {
 
 	fmt.Println("###BEGIN NEIGHBOR-JOINING")
 	time_start = time.Now().UnixMilli()
-	neighborJoin(distanceMatrix, S, labels, dead_record, array, treeBanana)
+	a, b := neighborJoin(distanceMatrix, S, labels, dead_record, array, treeBanana)
 	time_end = time.Now().UnixMilli()
 	time_neighborJoin := int(time_end - time_start)
 	fmt.Printf("###Done in %d milliseconds\n", time_neighborJoin)
 
-	if 1 == 2 {
+	if a == "" || b == nil {
 		t.Errorf(" failure :(")
 	}
 }
